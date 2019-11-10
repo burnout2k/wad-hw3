1fdaef32f52fcd9669d63b761fbf5d97cfcb908b
Allan Mitt <allan.mitt@gmail.com>
2019-11-10 23:54:52 +0200

    gpa calculation and keeping pages updated while swapping component

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -36,10 +36,15 @@
 
 <script>
 	import Course from "../models/Course";
+    import { bus } from '../main'
     export default {
         name: "Courses",
 		data: function() {
             return {
+                //gpa: 0,
+                testgpa : 0,
+                sum : 0,
+                howmanycourses : 0,
                 isActiveButton: false,courses: [
                     new Course("Operation systems", 1, 98),
                     new Course("Software Engineering", 1, 55),
@@ -67,8 +72,38 @@
 				var course = new Course(this.title, this.semester, this.grade);
 				this.courses.push(course);
 				this.cancel();
-			}
-        }
+				this.gpa();
+                bus.$emit('test', this.testgpa);
+            },
+            gpa: function() {
+                this.sum = 0;
+                this.howmanycourses = 0;
+                for (let i = 0; i < this.courses.length; i++) {
+                    if (this.courses[i].grade > 90){
+                        this.sum = this.sum + 4;
+                    }
+                    else if (this.courses[i].grade > 80){
+                        this.sum = this.sum + 3;
+                    }
+                    else if (this.courses[i].grade > 70){
+                        this.sum = this.sum + 2;
+                    }
+                    else if (this.courses[i].grade > 60){
+                        this.sum = this.sum + 1;
+                    }
+                    else if (this.courses[i].grade > 50){
+                        this.sum = this.sum + 0.5;
+                    }
+                    this.howmanycourses = this.howmanycourses + 1;
+                }
+                this.testgpa = this.sum/this.howmanycourses;
+            },
+            mounted(){
+                this.gpa();
+                bus.$emit('test', this.testgpa);
+            }
+
+        },
     }
 </script>
 
