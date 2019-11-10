b744533d08d161155bdc6eefe7fb2539f8cc583a
Allan Mitt <allan.mitt@gmail.com>
2019-11-11 01:48:02 +0200

    all done

---
+++ src/components/GPA.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="gpa">
+        <strong>{{ user.gpa }}</strong>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GPA",
+        props: {
+            user: Array,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <h1 class="title">Courses</h1>
+    <table id="courses">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Course Title</th>
+            <th>Semester</th>
+            <th>Grade</th>
+        </tr>
+        </thead>
+        <tbody v-for="(course, index) in courses" :key="index">
+        <tr>
+            <td>{{index+1}}</td>
+            <td>{{course.title}}</td>
+            <td>{{course.semester}}</td>
+            <td>{{course.grade}}</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "List",
+        props: {
+            courses: Array,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
